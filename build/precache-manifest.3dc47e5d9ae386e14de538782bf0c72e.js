1f18af4455d81944a0756781d9270235fb45ef0a
vtphu8 <ludz1516@gmail.com>
2022-12-01 23:35:05 +0700

    hoan thiet website vadeploy len firebase

---
+++ build/precache-manifest.3dc47e5d9ae386e14de538782bf0c72e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca1c5c3edc18982c73f9ec5b1b6c392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "600dabe1e0e9f4ef9fea",
+    "url": "/static/css/main.3ca34dfd.chunk.css"
+  },
+  {
+    "revision": "40394a2788b3bc40e164",
+    "url": "/static/js/2.e9d109ad.chunk.js"
+  },
+  {
+    "revision": "0c7bbd83885d4088b0424e023fce20ad",
+    "url": "/static/js/2.e9d109ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600dabe1e0e9f4ef9fea",
+    "url": "/static/js/main.ffcaacc5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ffcaacc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260705052eb5a5c8bd41",
+    "url": "/static/js/runtime-main.d3cf5db1.js"
+  }
+]);
